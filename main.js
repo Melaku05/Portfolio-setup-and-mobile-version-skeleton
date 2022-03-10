dcc57eefda14f1f5932b3068b8620b43ff8054d5
melaku <melakushiferaw05@gmail.com>
2022-03-10 12:31:38 +0300

    pop-up windows

--- main.js
+++ main.js
@@ -1,29 +1,123 @@
-/* eslint linebreak-style: ["error", "windows"] */
-
-const hamburgerButton = document.querySelector('.nav-btn');
-const slider = document.querySelector('.x-nav');
-const display = document.querySelector('.desktop-nav');
-const listEl = document.querySelectorAll('ul li');
 const body = document.querySelector('body');
+const menuDiv = document.createElement('div');
+menuDiv.className = 'menu-content';
+menuDiv.style.display = 'none';
 
-hamburgerButton.addEventListener('click', () => {
-  display.style.display = 'block';
-  hamburgerButton.style.display = 'nonev';
-  slider.style.display = 'block';
-  body.style.overflow = 'hidden';
+const htmlContent = `<img class="x-menu" src="images/Icon-Cancel.svg" alt="cancel image">
+<ul class="menu-list-item">
+  <li class="portfolio"><a class="menu-list-items" href="#portfolio">Portfolio</a></li>
+  <li class="about"><a class="menu-list-items" href="#about">About</a></li>
+  <li class="contact"><a class="menu-list-items" href="#contact">Contact</a></li>
+</ul>`;
+
+menuDiv.innerHTML = htmlContent;
+body.appendChild(menuDiv);
+const cancelImage = menuDiv.querySelector('img');
+cancelImage.className = 'x-menu';
+cancelImage.src = 'images/Icon-Cancel.svg';
+cancelImage.alt = 'cancel image';
+
+const menuIcon = document.querySelector('.menu');
+menuIcon.addEventListener('click', () => {
+  menuDiv.style.display = 'flex';
+  document.getElementById('header').style.filter = 'blur(0.3125rem)';
 });
 
-slider.addEventListener('click', () => {
-  display.style.display = 'none';
-  hamburgerButton.style.display = 'block';
-  slider.style.display = 'none';
-  body.style.overflow = 'scroll';
+cancelImage.addEventListener('click', () => {
+  menuDiv.style.display = 'none';
+  document.getElementById('header').style.filter = 'none';
 });
 
-listEl.forEach((clickForClose) => {
-  clickForClose.addEventListener('click', () => {
-    display.style.display = 'none';
-    hamburgerButton.style.display = 'block';
-    body.style.overflow = 'scroll';
+const itemLists = document.querySelectorAll('.menu-list-items');
+itemLists.forEach((item) => {
+  item.addEventListener('click', () => {
+    menuDiv.style.display = 'none';
+    document.getElementById('header').style.filter = 'none';
   });
+});
+
+const myProjects = [
+  {
+    name: 'Tonic',
+    description: `A daily selection of privately'personalized reads; no accounts or sign-ups
+    required.`,
+    featuredImage: './images/SnapshootDesktopPortfolioA.svg',
+    technology: ['html', 'css', 'javascript', 'github', 'ruby', 'bootstrap'],
+    liveVersion:
+      'https://melaku05.github.io/Portfolio-setup-and-mobile-version-skeleton/',
+    sourceCode:
+      'https://github.com/Melaku05/Portfolio-setup-and-mobile-version-skeleton',
+  },
+
+  {
+    name: 'Multi-Post Stories',
+    description: `Experimental content creation feature that allows users to add
+    to an existing story over the course of a day without
+    spamming their friends.`,
+    featuredImage: './images/SnapshootDesktopPortfolioB.svg',
+    technology: ['html', 'css', 'javascript', 'github', 'ruby', 'bootstrap'],
+    liveVersion:
+      'https://melaku05.github.io/Portfolio-setup-and-mobile-version-skeleton/',
+    sourceCode:
+      'https://github.com/Melaku05/Portfolio-setup-and-mobile-version-skeleton',
+  },
+
+  {
+    name: 'Facebook 360',
+    description:
+      "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR",
+    featuredImage: './images/SnapshootDesktopPortfolioC.svg',
+    technology: ['html', 'css', 'javascript', 'github', 'ruby', 'bootstrap'],
+    liveVersion:
+      'https://melaku05.github.io/Portfolio-setup-and-mobile-version-skeleton/',
+    sourceCode:
+      'https://github.com/Melaku05/Portfolio-setup-and-mobile-version-skeleton',
+  },
+
+  {
+    name: 'Uber Navigation',
+    description:
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    featuredImage: './images/SnapshootDesktopPortfolioD.svg',
+    technology: ['html', 'css', 'javascript', 'github', 'ruby', 'bootstrap'],
+    liveVersion:
+      'https://melaku05.github.io/Portfolio-setup-and-mobile-version-skeleton/',
+    sourceCode:
+      'https://github.com/Melaku05/Portfolio-setup-and-mobile-version-skeleton',
+  },
+];
+
+const seeProjectBtn = document.querySelectorAll('.see-project');
+seeProjectBtn.forEach((item, i) => {
+  item.addEventListener('click', () => {
+    const project = myProjects[i];
+    const modalMenu = document.querySelector('.modal-container');
+    const projectTitle = modalMenu.querySelector('.modal-card-title');
+    projectTitle.textContent = project.name;
+    const projectDesc = modalMenu.querySelectorAll('.modal-primary-text');
+    const [mobileDesc, desktopDesc] = projectDesc;
+    mobileDesc.textContent = project.description;
+    desktopDesc.textContent = project.description;
+    projectDesc.textContent = project.description;
+    const projectMobileImage = modalMenu.querySelector('.mobile-image');
+    projectMobileImage.src = project.featuredImage;
+    const projectDesktopImage = modalMenu.querySelector('.desktop-image');
+    projectDesktopImage.src = project.featuredImage;
+    const liveLink = document.querySelector('.live-link');
+    liveLink.href = project.liveVersion;
+    const liveSourceCode = document.querySelector('.source-code');
+    liveSourceCode.href = project.sourceCode;
+    const allTech = document.querySelectorAll('.modal-tag');
+    allTech.forEach((item, i) => {
+      item.textContent = project.technology[i];
+    });
+
+    modalMenu.style.display = 'flex';
+  });
+});
+
+const modalCancelBtn = document.querySelector('.modal-cancel-image');
+modalCancelBtn.addEventListener('click', () => {
+  const removeModalContainer = document.querySelector('.modal-container');
+  removeModalContainer.style.display = 'none';
 });
